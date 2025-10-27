1c6854e39ec33447d430c0daefad783784270ee5
jcrodriguez445 <juanksr208@gmail.com.com>
2025-10-27 16:02:03 -0500

    creación del .gitignore

---
+++ .gitignore.py
@@ -0,0 +1,22 @@
+# Entornos virtuales
+.venv/
+venv/
+
+# Archivos compilados de Python
+__pycache__/
+*.py[cod]
+*.pyo
+
+# Base de datos local
+*.db
+
+# Archivos temporales
+.env
+.env.*
+*.log
+*.sqlite
+
+# Archivos de entorno o configuración
+.DS_Store
+.idea/
+.vscode/
