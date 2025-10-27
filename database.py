ea43f9a54c2ef2054cec461c353598d35a89d36b
jcrodriguez445 <juanksr208@gmail.com.com>
2025-10-27 14:37:58 -0500

    (database) implementación de modelos para registrar metadata

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlmodel import SQLModel, create_engine
+import os
 
 sqlite_name = "database.db"
-engine = create_engine(f"sqlite:///{sqlite_name}", echo=False)
+sqlite_path = os.path.join(os.path.dirname(__file__), sqlite_name)
+engine = create_engine(f"sqlite:///{sqlite_path}", echo=False)
 
 def crear_bd():
-    from models import Estudiante, Curso, Matricula
+
+    from models import Estudiante, Curso, Matricula  # noqa: F401
     SQLModel.metadata.create_all(engine)
